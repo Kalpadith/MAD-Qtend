ac23b38f5aae0e98ad78b5a01b2347dd49307aa8
kalpadith <it20243180@my.sliit.lk>
2021-08-12 16:08:28 +0530

    Attendance part with menu

---
+++ app/src/main/java/com/example/mad_qtend/attendance.java
@@ -0,0 +1,41 @@
+package com.example.mad_qtend;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class attendance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_profile){
+
+        }
+        if (id == R.id.action_update){
+        Intent updateIntent = new Intent(attendance.this,Update_Attendance.class);
+        startActivity(updateIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
